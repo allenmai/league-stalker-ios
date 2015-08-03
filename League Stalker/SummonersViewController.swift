9a9915af838776d9e4c7e4f18b4aa88c509d3532
Allen Mai <mai25536@gmail.com>
2015-08-02 21:22:02 -0700

    Hi Summoners

---
+++ League Stalker/SummonersViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SummonersViewController.swift
+//  League Stalker
+//
+//  Created by Zheheng Mai on 8/2/15.
+//  Copyright © 2015 Zheheng Mai. All rights reserved.
+//
+
+import UIKit
+
+class SummonersViewController: UIViewController {
+
+}
